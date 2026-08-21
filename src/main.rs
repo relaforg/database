95ff7ed0a9cb2de93bda073e5d9f78e1ecf9e9bf
relaforg <laforgueremi@gmail.com>
2026-08-21 09:54:52 +0200

    Simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{Write, stdin, stdout};
+
+static PROMPT_TEXT: &str = "db";
+
+fn prompt() -> String {
+    let mut line = String::new();
+    print!("{} > ", PROMPT_TEXT);
+    if let Err(e) = stdout().flush() {
+        panic!("Failed to flush stdout: {}", e);
+    }
+    if let Err(e) = stdin().read_line(&mut line) {
+        panic!("Failed to read stdin: {}", e);
+    }
+    line.trim_end().to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let input = prompt();
+        if input.as_str() == ".exit" {
+            break;
+        } else {
+            println!("Invalid command");
+        }
+    }
 }
